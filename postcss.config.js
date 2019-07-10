4415821e3c1d15386b4ba8fc95ac1a39a623a88b
Carlos Coloma <ccescribano@gmail.com>
2019-07-10 13:51:32 +0100

    Update dependencies

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,5 @@
 const autoprefixer = require("autoprefixer");
 
 module.exports = {
-  plugins: [
-    autoprefixer({
-      browsers: ["last 2 version", "not dead"]
-    })
-  ]
+  plugins: [autoprefixer()]
 };
